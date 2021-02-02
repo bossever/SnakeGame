974a6fa17b36927881f23f03d12fc92520605bc3
Ayushman Sachan <ashusachan01@gmail.com>
2021-02-02 12:39:07 +0530

    Minor changes

---
+++ src/com/ayushman/GameFrame.java
@@ -0,0 +1,11 @@
+package com.ayushman;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+
+    public GameFrame() throws HeadlessException {
+        this.add(new GameFrame());
+    }
+}
